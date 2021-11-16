9cae22fb7ef76577a16f2488e30612718883507c
Eranga Wettewa <ehw89@msstate.edu>
2021-11-16 10:22:06 -0600

    first commit

--- 02_number_of_species.R
+++ 02_number_of_species.R
@@ -8,13 +8,13 @@
 library(tidyverse)
 library(janitor)
 library(lubridate)
-################################################################################
-# Import data
-df_ebd_ab_all = read_csv("./data/base/ebd_CA-AB_prv_relJan-2020_subset.csv")
-################################################################################
-# Tidying
-# Subset observations:
-################################################################################
+#------------------------------------------------------------------------------#
+# Import data                                                                  #
+df_ebd_ab_all = read_csv("./data/base/ebd_CA-AB_prv_relJan-2020_subset.csv")   #
+#------------------------------------------------------------------------------#
+# Tidying                                                                      #
+# Subset observations:                                                         #
+#------------------------------------------------------------------------------#
 df_ebd_ab_sub <- df_ebd_ab_all %>%
   # Clean up column names
   clean_names(case = "snake") %>%
@@ -24,25 +24,25 @@ df_ebd_ab_sub <- df_ebd_ab_all %>%
   select(common_name, scientific_name, county, iba_code, bcr_code,
          locality:observation_date, observer_id, sampling_event_identifier,
          group_identifier, trip_comments)
-################################################################################
-# Retrieve number of checklists and unique species for each locality
-################################################################################
+#------------------------------------------------------------------------------#
+# Retrieve number of checklists and unique species for each locality           #
+#------------------------------------------------------------------------------#
 df_loc_check <- df_ebd_ab_sub %>%
   mutate(month = month(observation_date)) %>%
   filter(locality_type == "H" | locality_type == "P") %>%
   group_by(locality, locality_id, month) %>%
   summarise(n_checklist = n_distinct(sampling_event_identifier),
             n_species = n_distinct(scientific_name))
-################################################################################
+#------------------------------------------------------------------------------#
 # remove locality name                                                         #
-################################################################################
+#------------------------------------------------------------------------------#
 df_loc_check = subset(df_loc_check,select = -c(locality, n_checklist))
 df_loc_check <- df_loc_check%>%
   group_by(locality_id)
 write_csv(df_loc_check, "data/processed/n_species_monthly.csv")
-################################################################################
+#------------------------------------------------------------------------------#
 #number of species per hot spot                                                #
-################################################################################
+#------------------------------------------------------------------------------#
 df_unique_species_loc = subset(df_ebd_ab_sub, select = c(scientific_name, locality_id, sampling_event_identifier))%>%
   group_by(locality_id)%>%
   summarise(n_checklist = n_distinct(sampling_event_identifier),
